f58901e6f0231390751805fac50d264bd1c5ca75
aman <aman091299@gmail.com>
2025-05-03 13:06:14 +0530

    adding vercel url for frontend cors issue

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ require('dotenv').config()
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000','https://dev-tinder-frontend-psi.vercel.app'],
   credentials: true,
 };
 
