ec49617d4b21ed585cf315d3aac2b71673125b7c
aman <aman091299@gmail.com>
2025-05-03 22:47:12 +0530

    addig

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -41,7 +41,7 @@ authRouter.post("/signup", async (req, res) => {
   expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 24 hours
   secure: true,          // Required for HTTPS
       sameSite: 'None',      // Required for cross-site
-      domain: 'dev-tinder-frontend-psi.vercel.app', // Your specific domain
+  
       httpOnly: false,        // Security best practice
       path: '/',            // Available on all paths
 }); 
@@ -82,7 +82,7 @@ authRouter.post('/login',async(req,res)=>{
     expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 24 hours
     secure: true,          // Required for HTTPS
     sameSite: 'None',      // Required for cross-site
-    domain: 'dev-tinder-frontend-psi.vercel.app', // Your specific domain
+  
     httpOnly: false,        // Security best practice
     path: '/',          // Available on all paths
   });
@@ -104,7 +104,7 @@ authRouter.post('/logout',(req,res)=>{
   res.clearCookie('token', {
     secure: true,          // Required for HTTPS
     sameSite: 'None',      // Required for cross-site
-    domain: 'dev-tinder-frontend-psi.vercel.app', // Your specific domain
+   
     httpOnly: false,        // Security best practice
     path: '/',         // Must match if originally set
   });
